4e7b05d1b349569bc3400e375316a201813202e5
L1b3rtyghxst <104371193+L1b3rtyghxst@users.noreply.github.com>
2024-04-25 08:32:32 +0300

    Create jobs.py

---
+++ WEB. Flask-sqlalchemy /data/jobs.py
@@ -0,0 +1,31 @@
+import sqlalchemy as sa
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+association_table = sa.Table("jobs_to_categories", SqlAlchemyBase.metadata,
+                             sa.Column("job_id", sa.Integer, sa.ForeignKey("jobs.id")),
+                             sa.Column("category.id", sa.Integer, sa.ForeignKey("categories.id")))
+
+
+class Category(SqlAlchemyBase):
+    __tablename__ = "categories"
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String)
+
+
+class Jobs(SqlAlchemyBase): 
+    __tablename__ = 'jobs'
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    team_leader_id = sa.Column(sa.Integer, sa.ForeignKey('users.id')) 
+    team_leader = orm.relationship("User")
+    job = sa.Column(sa.Text)  
+    work_size = sa.Column(sa.Integer) 
+    collaborators = sa.Column(sa.String) 
+    start_date = sa.Column(sa.Date)
+    end_date = sa.Column(sa.Date)
+    categories = orm.relationship("Category", secondary=association_table, backref="jobs")
+    is_finished = sa.Column(sa.Boolean, default=False)
+
+    def __repr__(self):
+        return f"<Jobs {self.id} {self.job} {self.is_finished}>"
